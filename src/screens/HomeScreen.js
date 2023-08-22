48aedfbb19178da9ce0900336a297d3e4e0eb55b
Rimao416 <omarkayumba12345@gmail.com>
2023-08-22 14:30:09 +0100

    Exercice01

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,22 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
+  const myName = "Omari";
   return (
     <View>
-      <Text style={styles.textStyle}>AIME MOI</Text>
+      <Text style={styles.textStyle}>Getting Started with react native</Text>
+      <Text style={styles.secondStyle}> My Name is {myName}</Text>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   textStyle: {
+    fontSize: 45,
+  },
+  secondStyle: {
     fontSize: 20,
-
   },
 });
+
 export default HomeScreen;

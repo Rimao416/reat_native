516e55e99454729f44a459ca0c780d801c124f8d
Rimao416 <omarkayumba12345@gmail.com>
2023-08-22 14:46:56 +0100

    Boutton

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   const myName = "Omari";
+  const handlePress = () => {
+    console.log("Le bouton a été appuyé !");
+  };
   return (
     <View>
       <Text style={styles.textStyle}>Getting Started with react native</Text>
-      <Text style={styles.secondStyle}> My Name is {myName}</Text>
+      <Button onPress={() => console.log(myName)} title="Click Me"></Button>
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Go to The List demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
